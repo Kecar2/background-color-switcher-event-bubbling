47a00adaff5efc63808c25a8624afa35ff82c703
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-03-24 10:13:30 +0100

    done

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 // Happy coding!
+// Happy coding!
 
+const swatches = document.querySelector(".canvas");
+
+swatches.addEventListener("click", function (event) {
+
+    // currentTarget
+    console.log("currentTarget", event.currentTarget);
+
+    // target
+    console.log("target", event.target);
+
+    //document.body.style.backgroundColor = event.target.id;
+    document.body.style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+});
